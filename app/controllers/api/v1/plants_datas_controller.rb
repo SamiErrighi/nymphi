c12baa31f982f74750f34fae8cd09e13acf42085
SamiErrighi <sami.errighi@gmail.com>
2015-04-09 22:48:03 +0200

    try fix bug

--- app/controllers/api/v1/plants_datas_controller.rb
+++ app/controllers/api/v1/plants_datas_controller.rb
@@ -16,6 +16,14 @@ module Api
       def info
         water = Humidity::min_max
         light = Luminosity::min_max
+        render json: {
+            'minWater': water[0],
+            'maxWater': water[1],
+            'minLight': light[0],
+            'maxLight': light[1],
+            #'minTemp': Profile.first.plant.temperature_min,
+            #'maxTemp': Profile.first.plant.temperature_max,
+        }
       end
     end
   end
